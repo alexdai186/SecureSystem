d2c9922910aa4066a1bd3e56ab0b951ff6dcd5ff
cindywu2018 <cindywu2018@utexas.edu>
2017-02-07 13:11:01 -0600

    added enums for InstructionType and security level, parsed args somewhat, initial outline of reference monitor

--- ReferenceMonitor.java
+++ ReferenceMonitor.java
@@ -1,42 +1,31 @@
 import java.util.ArrayList;
-
-/**
- * Created by Alex Dai on 2/7/2017.
- */
-
-
-/* S_Object Object */
-// Documentation suggests we do this in ReferenceMonitor Class
-class S_Object {
-    String name;
-    String security_level;
-    int temp = 0;
-
-    public S_Object(String n, String s, int t){
-        name = n;
-        security_level = s;
-        temp = t;
-    }
-
-    public void setTemp(int t){
-        temp = t;
-    }
-}
-
-
 public class ReferenceMonitor {
 
-    ArrayList<S_Object> obj_list;
-    public ReferenceMonitor(){
+    private ArrayList<S_Object> obj_list;
+
+    public ReferenceMonitor() {
         obj_list = new ArrayList<>();
     }
-    public void createObject(String name, String security_level) {
-        /* Possible issue with object names? */
-        S_Object temp = new S_Object(name, security_level, 0);
+    public void createObject(String name, SecurityLevel level) {
+        S_Object temp = new S_Object(name, level);
         obj_list.add(temp);
     }
-
     public ArrayList<S_Object> getObjects() {
         return obj_list;
+    }
+
+    class S_Object {
+        private String name;
+        private String securityLevel;
+        private int temp = 0;
+
+        public S_Object(String n, SecurityLevel level) {
+            name = n;
+            securityLevel = s;
+        }
+
+        public void setTemp(int t){
+            temp = t;
+        }
     }
 }
--- SecureSystem.java
+++ SecureSystem.java
@@ -1,106 +1,170 @@
-/**
- * Created by Alex Dai on 2/5/2017.
- */
 import java.lang.String;
 import java.lang.Integer;
-import java.util.ArrayList;
+import java.util.*;
+import ReferenceMonitor.class;
 
-
-/* Instruction Object */
-class InstructionObject {
-    String command;
-    S_Subject subject;
-    S_Object object;
-    int value;
-
-    public InstructionObject(){}
+// An enum for InstructionType
+public enum InstructionType {
+    READ("read"),
+    WRITE("write"),
+    BAD("bad");
+    private String value;
+    private InstructionType(String v) {
+        value = v;
+    }
+    public String getValue() {
+        return value;
+    }
 }
 
-/* S_Subject Object */
-class S_Subject {
-    String name;
-    String security_level;
-    int temp = 0;
-    public S_Subject(){}
+// SecurityLevel should be a class with a defined "dominates" relation.
+public enum SecurityLevel {
+    // HOW TO ESTABLISH DOMINANCE????????????????????????????
+    LOW("low"),
+    HIGH("high");
+    private String level;
+    private SecurityLevel(String l) {
+        level = l;
+    }
+    public String getLevel() {
+        return level;
+    }
+
 }
 
+/**
+ * InstructionObject class
+ * Stores type of instruction (read, write, bad), name of subject, name of object, and optional value
+ */
+public class InstructionObject {
+    private InstructionType insType;
+    private String subjectName;
+    private String objectName;
+    private int value;
 
-public class SecureSystem extends ReferenceMonitor{
+    // Constructors
+    public InstructionObject() {}
+    public InstructionObject(InstructionType i, String sName, String oName) {
+        insType = i;
+        subjectName = sName;
+        objectName = oName;
+        value = v;
+    }
+    // Setters
+    public void setValue(int v) {
+        value = v;
+    }
 
-    public static void main(String[] args){
+    public void setInstructionType(InstructionType i) {
+        insType = i;
+    }
 
-        /* Create Subjects */
-        S_Subject hal = new S_Subject();
-        hal.name = "hal";
-        hal.security_level = "HIGH";
+    public void setSubject(String s) {
+        subjectName = s;
+    }
 
-        S_Subject lyle = new S_Subject();
-        lyle.name = "lyle";
-        lyle.security_level = "LOW";
+    public void setObject(String o) {
+        objectName = o;
+    }
 
-        /* Create Objects */
+    // Getters
+    public InstructionType getInstructionType() {
+        return insType;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+
+public class SecureSystem {
+    private ArrayList<S_Subject> subjList; 
+    
+    public class S_Subject {
+        private String name;
+        private String securityLevel;
+        private int temp = 0;
+        public S_Subject(String n, SecurityLevel level) {
+            name = n;
+            securityLevel = level;
+        }
+
+        public void setTemp(int t) {
+            temp = t;
+        }
+    }
+
+    static public InstructionObject parseArgs(String[] args){
+        InstructionObject insObject = new InstructionObject();        
+        // check what the command is
+        String type = args[0].toLowerCase();
+        switch(type) {
+            case "read":
+                // checks if read command is illegal
+                if (args.length != 3) {
+                    insObject.setInstructionType(InstructionType.BAD);
+                    // return insObject;
+                }
+                else {
+                    insObject.setInstructionType(InstructionType.READ);
+                }
+                break;
+            case "write":
+                // checks if write command is illegal
+                if (args.length != 4) {
+                    insObject.setInstructionType(InstructionType.BAD);
+                    // return insObject;
+                }
+                else {
+                    try {
+                        insObject.setValue(Integer.parseInt(args[3]));
+                    }
+                    catch(Exception e) {
+                        insObject.setInstructionType(InstructionType.BAD);
+                        return insObject;
+                    }
+                    insObject.setInstructionType(InstructionType.WRITE);
+                }
+                break;
+            default:
+                // bad command, not a read/write
+                insObject.setInstructionType(InstructionType.BAD);
+                break;
+        }
+
+        if (insObject.getInstructionType() == InstructionType.BAD)
+            return insObject;
+
+        /* Sets Subject for Instruction */
+        insObject.setSubject(args[1].toLowerCase());
+        insObject.setObject(args[2].toLowerCase());
+        
+        return insObject;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // Create two new subjects
+        S_Subject hal = new S_Subject("hal", SecurityLevel.HIGH);
+        S_Subject lyle = new S_Subject("lyle", SecurityLevel.LOW);
+
+        // Create two new objects
         ReferenceMonitor sys = new ReferenceMonitor();
         sys.createObject("lobj", "LOW");
         sys.createObject("hobj", "HIGH");
 
+        subjList = new ArrayList<S_Subject>();
+        subjList.add(hal);
+        subjList.add(lyle);
 
-        parseArgs(args, sys, hal, lyle);
-
-    }
-
-    static public InstructionObject parseArgs(String[] args, ReferenceMonitor sys,
-                                              S_Subject hal, S_Subject lyle){
-
-        InstructionObject bad = new InstructionObject();
-        bad.command = "bad";
-        /* Returns BadInstruction if invalid number of arguments */
-        if (args.length < 3 || args.length > 4)
-            return bad;
-
-        /* Sets the Command for Instruction*/
-        String command = args[0].toLowerCase();
-        InstructionObject ins = new InstructionObject();
-        switch (command){
-            case "read":
-                ins.command = "read";
-                break;
-            case "write":
-                ins.command = "write";
-            default:
-                return bad;
-        }
-
-        /* Sets Subject for Instruction */
-        String arg1 = args[1].toLowerCase();
-        if (arg1.equals("lyle")) {
-            ins.subject = lyle;
-        }
-        else if (arg1.equals("hal")) {
-            ins.subject = hal;
-        }
-
-        /* Sets Object for Instruction */
-        String arg2 = args[2].toLowerCase();
-        ArrayList<S_Object> obj_list = sys.getObjects();
-        for (S_Object o : obj_list){
-            if (arg2.equals(o.name))
-                ins.object = o;
-                break;
-        }
-        if (ins.object == null)
-            return bad;
-
-        /* If command is a "write" command there must be a 4th argument */
-        if (args.length > 3 && ins.command.equals("write")){
-            try{
-                ins.value = Integer.parseInt(args[3]);
-            }
-            catch(Exception e){
-                return bad;
-            }
-        }
-        else
-            return bad;
-        return ins;
+        parseArgs(args);
     }
 }
