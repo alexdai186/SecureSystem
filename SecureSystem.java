0b8ce0013cf08fd95f6d7125feffdfffa53c16c1
Alex Dai <peep186@gmail.com>
2017-02-06 21:43:54 -0600

    Added stuff to SecureSystem. Created Unit Test Suite for Secure System

--- SecureSystem.java
+++ SecureSystem.java
@@ -4,33 +4,62 @@
 import java.lang.String;
 import java.lang.Integer;
 
-class InstructionObject{
+
+/* Instruction Object */
+class InstructionObject {
     String command;
-    String subject;
-    String object;
+    S_Subject subject;
+    S_Object object;
     int value;
+
     public InstructionObject(){}
+}
+
+/* S_Subject Object */
+class S_Subject {
+    String name;
+    String security_level;
+
+    public S_Subject(){}
+}
+
+/* S_Object Object */
+// Documentation suggests we do this in ReferenceMonitor Class
+class S_Object {
+    String name;
+    String security_level;
+
+    public S_Object(){}
 }
 
 public class SecureSystem {
 
     public static void main(String[] args){
 
+        S_Subject hal = new S_Subject();
+        hal.name = "hal";
+        hal.security_level = "HIGH";
+
+        S_Subject lyle = new S_Subject();
+        lyle.name = "lyle";
+        lyle.security_level = "LOW";
+
+
+
         parseArgs(args);
 
     }
 
     static public InstructionObject parseArgs(String[] args){
-        //Checks len of arguments
         InstructionObject bad = new InstructionObject();
         bad.command = "bad";
-        if (args.length < 3 || args.length > 4){
+        /* Returns BadInstruction if invalid number of arguments */
+        if (args.length < 3 || args.length > 4)
             return bad;
-        }
 
-        InstructionObject ins = new InstructionObject();
-        /* Sets the Command */
+        /* Sets the Command for Instruction*/
         String command = args[0].toLowerCase();
+        InstructionObject ins = new InstructionObject();
         switch (command){
             case "read":
                 ins.command = "read";
@@ -40,8 +69,15 @@ public class SecureSystem {
             default:
                 return bad;
         }
-        ins.subject = args[1].toLowerCase();
+
+        /* Sets User and Target for Instruction */
+        String arg1 = args[1].toLowerCase();
+        String arg2 = args[2].toLowerCase();
+        if (arg1.equals("lyle") || arg1.equals("hal")){
+            ins.subject = arg1;
+        }
         ins.object = args[2].toLowerCase();
+
         if (args.length > 3){
             try{
                 ins.value = Integer.parseInt(args[3]);
---
+++ SecureSystemTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Alex Dai on 2/6/2017.
+ */
+public class SecureSystemTest {
+
+}
