b4bdd6ba3feaa8c784f9ba43aa71e80f181b5b15
Alex Dai <peep186@gmail.com>
2017-02-07 13:07:13 -0600

    Added Test Cases

--- SecureSystemTest.java
+++ SecureSystemTest.java
@@ -5,4 +5,44 @@ import static org.junit.Assert.*;
  */
 public class SecureSystemTest {
 
+    public void test_parseArgs1(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"write", "hal", "lyle", "10"};
+        assertEquals("write", sys.parseArgs(args).getInstructionType().getValue());
+        assertEquals("hal", sys.parseArgs(args).getSubject());
+        assertEquals("lyle", sys.parseArgs(args).getObject());
+        assert(10, sys.parseArgs(args).getValue());
+    }
+
+    public void test_parseArgs2(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"write", "hal", "lyle", "hello"};
+        assertEquals("bad", sys.parseArgs(args).getInstructionType().getValue());
+    }
+
+    public void test_parseArgs3(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"read", "hal8", "lyle"};
+        assertEquals("read", sys.parseArgs(args).getInstructionType().getValue());
+        assertEquals("hal8", sys.parseArgs(args).getSubject());
+        assertEquals("lyle", sys.parseArgs(args).getObject());
+    }
+
+    public void test_parseArgs4(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"read"};
+        assertEquals("bad", sys.parseArgs(args).getInstructionType().getValue());
+    }
+
+    public void test_parseArgs5(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"read", "read", "read", "read"};
+        assertEquals("bad", sys.parseArgs(args).getInstructionType().getValue());
+    }
+
+    public void test_parseArgs6(){
+        SecureSystem sys = new SecureSystem();
+        String[] args = {"hi", "hi", "hi"};
+        assertEquals("bad", sys.parseArgs(args).getInstructionType().getValue());
+    }
 }
